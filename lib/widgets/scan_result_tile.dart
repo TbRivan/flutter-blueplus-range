b489f74de7ec9a54d6b8112cf1e4be5c53200458
tbrivan <tbrivan22@gmail.com>
2024-02-07 15:48:26 +0700

    update result tile

--- lib/widgets/scan_result_tile.dart
+++ lib/widgets/scan_result_tile.dart
@@ -40,28 +40,6 @@ class _ScanResultTileState extends State<ScanResultTile> {
     super.dispose();
   }
 
-  String getNiceHexArray(List<int> bytes) {
-    return '[${bytes.map((i) => i.toRadixString(16).padLeft(2, '0')).join(', ')}]';
-  }
-
-  String getNiceManufacturerData(List<List<int>> data) {
-    return data
-        .map((val) => '${getNiceHexArray(val)}')
-        .join(', ')
-        .toUpperCase();
-  }
-
-  String getNiceServiceData(Map<Guid, List<int>> data) {
-    return data.entries
-        .map((v) => '${v.key}: ${getNiceHexArray(v.value)}')
-        .join(', ')
-        .toUpperCase();
-  }
-
-  String getNiceServiceUuids(List<Guid> serviceUuids) {
-    return serviceUuids.join(', ').toUpperCase();
-  }
-
   bool get isConnected {
     return _connectionState == BluetoothConnectionState.connected;
   }
@@ -93,58 +71,42 @@ class _ScanResultTileState extends State<ScanResultTile> {
         backgroundColor: Colors.black,
         foregroundColor: Colors.white,
       ),
-      onPressed:
-          (widget.result.advertisementData.connectable) ? widget.onTap : null,
-      child: isConnected ? const Text('OPEN') : const Text('CONNECT'),
+      onPressed: widget.onTap,
+      child: const Text('INFO'),
     );
   }
 
-  Widget _buildAdvRow(BuildContext context, String title, String value) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(title, style: Theme.of(context).textTheme.bodySmall),
-          const SizedBox(
-            width: 12.0,
-          ),
-          Expanded(
-            child: Text(
-              value,
-              style: Theme.of(context)
-                  .textTheme
-                  .bodySmall
-                  ?.apply(color: Colors.black),
-              softWrap: true,
-            ),
-          ),
-        ],
-      ),
+  Widget _buildIconSignal(icon, Color color, rssi) {
+    return Tooltip(
+      message: 'Signal Strength: $rssi',
+      triggerMode: TooltipTriggerMode.tap,
+      showDuration: const Duration(seconds: 1),
+      child: Icon(icon, color: color),
     );
+  }
+
+  Widget _buildSignalIndicator(BuildContext context) {
+    var rssi = widget.result.rssi.abs();
+    if (rssi <= 50) {
+      return _buildIconSignal(Icons.signal_cellular_alt_rounded,
+          const Color.fromARGB(200, 0, 200, 0), rssi);
+    } else if (rssi > 50 && rssi <= 60) {
+      return _buildIconSignal(Icons.signal_cellular_alt_2_bar_rounded,
+          const Color.fromARGB(200, 200, 200, 0), rssi);
+    } else if (rssi > 60) {
+      return _buildIconSignal(Icons.signal_cellular_alt_1_bar_rounded,
+          const Color.fromARGB(200, 200, 0, 0), rssi);
+    } else {
+      return const Icon(Icons.signal_wifi_off);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var adv = widget.result.advertisementData;
-    return ExpansionTile(
+    return ListTile(
       title: _buildTitle(context),
-      leading: Text(widget.result.rssi.toString()),
+      leading: _buildSignalIndicator(context),
       trailing: _buildConnectButton(context),
-      children: <Widget>[
-        if (adv.advName.isNotEmpty) _buildAdvRow(context, 'Name', adv.advName),
-        if (adv.txPowerLevel != null)
-          _buildAdvRow(context, 'Tx Power Level', '${adv.txPowerLevel}'),
-        if (adv.msd.isNotEmpty)
-          _buildAdvRow(
-              context, 'Manufacturer Data', getNiceManufacturerData(adv.msd)),
-        if (adv.serviceUuids.isNotEmpty)
-          _buildAdvRow(
-              context, 'Service UUIDs', getNiceServiceUuids(adv.serviceUuids)),
-        if (adv.serviceData.isNotEmpty)
-          _buildAdvRow(
-              context, 'Service Data', getNiceServiceData(adv.serviceData)),
-      ],
     );
   }
 }
